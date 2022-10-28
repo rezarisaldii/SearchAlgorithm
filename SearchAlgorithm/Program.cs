867e6100249ad5bb2cfb3820cdec83ef657d5250
RezaRisaldi <reza.risaldi.ft21@mail.umy.ac.id>
2022-10-28 16:48:08 +0700

    public void LinearSearch

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -81,6 +81,34 @@ namespace SearchAlgorithm
 
             } while ((ch == 'y') || (ch == 'Y'));
         }
+        public void LinearSearch()
+        {
+            char ch;
+            //Search for number of comparison
+            int ctr;
+            do
+            {
+                //accept the number to be searched
+                Console.Write("\nEnter the element you want to search: ");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                ctr = 0;
+                for (i = 0; i < n; i++)
+                {
+                    ctr++;
+                    if (arr[i] == item)
+                    {
+                        Console.WriteLine("\n" + item.ToString() + "found st position" + (i + 1).ToString());
+                        break;
+                    }
+                }
+                if (i == n)
+                    Console.WriteLine("\n" + item.ToString() + "not found in the array");
+                Console.WriteLine("\nNumber of comparison: " + ctr);
+                Console.Write("\nContinue search (y/n): ");
+                ch = char.Parse(Console.ReadLine());
+            } while ((ch == 'y') || (ch == 'Y'));
+        }
 
         static void Main(string[] args)
         {
