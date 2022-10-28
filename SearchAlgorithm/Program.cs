bc2ee35caac3749594e4621844279b5024a1e2cb
RezaRisaldi <reza.risaldi.ft21@mail.umy.ac.id>
2022-10-28 16:12:42 +0700

    Accept Array Element

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -19,13 +19,25 @@ namespace SearchAlgorithm
         {
             while(true)
             {
-                Console.Write("Enter the number of elements in the array");
+                Console.Write("Enter the number of elements in the array: ");
                 string s = Console.ReadLine(); 
                 n = int.Parse(s);
                 if ((n > 0) && (n <= 20))
                     break;
                 else
-                    Console.WriteLine();
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 elements");
+            }
+            //accept array elements 
+            Console.WriteLine("");
+            Console.WriteLine("----------------------");
+            Console.WriteLine(" Enter Array elements ");
+            Console.WriteLine("----------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                String s1 = Console.ReadLine();
+                arr[i] = int.Parse(s1);
+
             }
         }
 
